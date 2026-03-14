fd7bd5c39bb5e2a93cfcfb1b3839ff3d1605d116
白阳 <839336369@qq.com>
2026-03-15 01:17:21 +0800

    1

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,7 +1,11 @@
 export type { RequestProtocol, ResponseProtocol } from './types/Protocol';
 export { createRequestProtocol, parseResponseProtocol } from './types/Protocol';
-export type { GlobalConfig, ClassConfig, MethodConfig } from './decorators/Annotations';
-export { MetaClass, MetaMethod, Send, Receive, getClassConfig, getMethodConfig, hasSendConfig } from './decorators/Annotations';
+export type { NetConfig, GlobalConfig } from './decorators/Global';
+export { GlobalNet } from './decorators/Global';
+export type { ClassConfig } from './decorators/Class';
+export { MetaClass, getClassConfig } from './decorators/Class';
+export type { MethodConfig } from './decorators/Method';
+export { MetaMethod, Send, Receive, getMethodConfig, hasSendConfig } from './decorators/Method';
 export type { SerializeLang, SendConfig, ReceiveConfig } from './serialize/Serialize';
 export { 
   SerializeException, 
